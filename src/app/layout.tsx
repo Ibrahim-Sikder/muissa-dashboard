ded598b06a063126118a1fae63f4eff756c2510b
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-06-01 01:32:48 +0600

    muissa-project-setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Providers from "@/lib/Providers";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <Providers>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toaster position="bottom-right" richColors />
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </body>
     </html>
+  </Providers>
   );
 }
