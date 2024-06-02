9842ba6548a5a9be767ac82d33494399ef66cc81
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-06-01 20:52:40 -0400

    new-service

--- src/components/ui/HomePage/Services/Service.tsx
+++ src/components/ui/HomePage/Services/Service.tsx
@@ -1,22 +1,39 @@
 import { Button } from "@mui/material";
 import React from "react";
 import Container from "../Container/Container";
+import { BusinessCenter } from "@mui/icons-material";
 
 const Service = () => {
+  const serviceData = [
+    {
+      id: 1,
+      title: "Product Services ",
+      description:
+        "Products quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet.",
+    },
+    {
+      id: 1,
+      title: "Product Services ",
+      description:
+        "Products quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet.",
+    },
+    {
+      id: 1,
+      title: "Product Services ",
+      description:
+        "Products quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet.",
+    },
+    {
+      id: 1,
+      title: "Product Services ",
+      description:
+        "Products quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet.",
+    },
+  ];
   return (
     <Container>
       <div className="serviceCardWraps">
-        <div className="serviceCard">
-          <div className="serviceIconWraps"></div>
-         <div className="serviceContent">
-         <h4>Asset management </h4>
-          <p className="leading-8 my-5">
-            Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt.
-            Duis leo. Sed fringilla mauris sit amet.
-          </p>
-          <Button>Read More </Button>
-         </div>
-        </div>
+       div.grid.grid-cols-
       </div>
     </Container>
   );
