493631cc475bc13e3b25c91039ac843b581fc1e8
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-06-06 14:44:40 +0600

    merge-problem-fixed

--- src/app/(dashboardLayout)/dashboard/policies/edit/page.tsx
+++ src/app/(dashboardLayout)/dashboard/policies/edit/page.tsx
@@ -1,23 +1,36 @@
-import CreateBlog from "@/components/Dashboard/pages/blogs/CreateBlog";
-import CreateFAQForm from "@/components/Dashboard/pages/faq/FaqForm";
-import EditPoliciesForm from "@/components/Dashboard/pages/policies/EditPoliciesForm";
-import CreateReview from "@/components/Dashboard/pages/reviews/CreateReview";
-import { Stack, Typography } from "@mui/material";
-import type { Metadata } from "next";
+// import CreateBlog from "@/components/Dashboard/pages/blogs/CreateBlog";
+// import CreateFAQForm from "@/components/Dashboard/pages/faq/FaqForm";
+// import EditPoliciesForm from "@/components/Dashboard/pages/policies/EditPoliciesForm";
+// import CreateReview from "@/components/Dashboard/pages/reviews/CreateReview";
+// import { Stack, Typography } from "@mui/material";
+// import type { Metadata } from "next";
 
-export default function EditPoliciesPage() {
+// export default function EditPoliciesPage() {
+//   return (
+//     <Stack spacing={3}>
+//       <div>
+//         <Typography variant="h4">Edit Policies</Typography>
+//       </div>
+//       <EditPoliciesForm />
+//     </Stack>
+//   );
+// }
+
+// export const metadata: Metadata = {
+//   title: "Muissa Consulting | Policies ",
+//   description: "Muissa Consulting blogs page ",
+//   keywords: "blogs, Muissa Consulting",
+// };
+
+
+import React from 'react';
+
+const page = () => {
   return (
-    <Stack spacing={3}>
-      <div>
-        <Typography variant="h4">Edit Policies</Typography>
-      </div>
-      <EditPoliciesForm />
-    </Stack>
+    <div>
+      <h4>policy</h4>
+    </div>
   );
-}
-
-export const metadata: Metadata = {
-  title: "Muissa Consulting | Policies ",
-  description: "Muissa Consulting blogs page ",
-  keywords: "blogs, Muissa Consulting",
 };
+
+export default page;
