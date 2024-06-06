bd69487e98949acb651fb4902f53644a810a1851
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-06-06 10:29:50 +0600

    verify-otp

--- src/app/(mainLayout)/verify/page.tsx
+++ src/app/(mainLayout)/verify/page.tsx
@@ -1,77 +1,119 @@
 "use client";
-
-import MUIForm from "@/components/Forms/Form";
-import MUIInput from "@/components/Forms/Input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { LockPerson, VerifiedUser } from "@mui/icons-material";
-import { Box, Button, Stack } from "@mui/material";
+import lockIcon from "../../../assets/icon/lock-icon.svg";
+import { Button } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent, KeyboardEvent, useRef, useState } from "react";
 import { FieldValues } from "react-hook-form";
-import { z } from "zod";
+import { toast } from "sonner";
 
-const validationSchema = z.object({
-  otp: z.string().min(4, "Please enter your 6 digit OTP"),
-});
+const OTPInput = ({
+  length,
+  setOTPValues,
+  emptyFieldIndex,
+  inputsRef,
+}: {
+  length: number;
+  setOTPValues: React.Dispatch<React.SetStateAction<string[]>>;
+  emptyFieldIndex: number | null;
+  inputsRef: React.MutableRefObject<HTMLInputElement[]>;
+}) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, idx: number) => {
+    const { value } = e.target;
+    setOTPValues((prevValues) => {
+      const updatedValues = [...prevValues];
+      updatedValues[idx] = value;
+      return updatedValues;
+    });
 
-const VerifyPage = () => {
-  const onSubmit = async (values: FieldValues) => {
-    // console.log(values);
+    // If the current input is not the last one and it's not empty
+    if (idx < length - 1 && value.length === 1) {
+      // Focus on the next input field
+      inputsRef.current[idx + 1]?.focus();
+    }
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>, idx: number) => {
+    if (e.key === "Backspace" && !e.currentTarget.value && idx > 0) {
+      console.log("Moving focus to previous input");
+      inputsRef.current[idx - 1]?.focus();
+    }
   };
 
   return (
-    <Stack
-      sx={{
-        alignItems: "center",
-        justifyContent: "center",
-        height: { sm: "100vh" },
-      }}
-    >
-      <Box
-        sx={{
-          px: 4,
-          py: 2,
-          maxWidth: 400,
-          width: "100%",
-          boxShadow: 1,
-          borderRadius: 1,
-          padding: 5,
-          margin: "0 auto",
-        }}
-      >
-        <Stack alignItems="center" justifyContent="center">
-          <Box
-            sx={{
-              "& svg": {
-                width: 80,
-                height: 80,
-              },
-            }}
-          >
-            <LockPerson sx={{ color: "primary.main" }} />
-          </Box>
-        </Stack>
-
-        <MUIForm
-          onSubmit={onSubmit}
-          resolver={zodResolver(validationSchema)}
-          defaultValues={{
-            otp: " ",
+    <div className="flex justify-between">
+      {Array.from({ length }, (_, idx) => (
+        <input
+          key={idx}
+          type="text"
+          maxLength={1}
+          className={`border rounded w-10 h-10 text-center text-lg mx-1 ${
+            emptyFieldIndex === idx ? "border-red-500" : "border-gray-300"
+          }`}
+          onChange={(e) => handleChange(e, idx)}
+          onKeyDown={(e) => handleKeyDown(e, idx)}
+          //@ts-ignore
+          ref={(el) => (inputsRef.current[idx] = el as HTMLInputElement)}
+          style={{
+            outline: "none",
+            boxShadow: "none",
           }}
-        >
-          <Box sx={{ width: "100%", mb: 2 }}>
-            <MUIInput type="text" name="otp" label="Enter Your OTP" fullWidth />
-          </Box>
-
-          <Box
-            sx={{ width: "100%", display: "flex", justifyContent: "center" }}
-          >
-            <Button type="submit" sx={{ width: "100%" }}>
-              Verify OTP
-            </Button>
-          </Box>
-        </MUIForm>
-      </Box>
-    </Stack>
+          onFocus={(e) => (e.target.style.borderColor = "#059669")}
+          onBlur={(e) => (e.target.style.borderColor = "#ccc")}
+        />
+      ))}
+    </div>
   );
 };
 
-export default VerifyPage;
+const OTPVerifyPage = () => {
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const searchParams = useSearchParams();
+  const userEmail = searchParams.get("email");
+  const router = useRouter();
+  const [otpValues, setOTPValues] = useState<string[]>(new Array(6).fill(""));
+  const [emptyFieldIndex, setEmptyFieldIndex] = useState<number | null>(null);
+  const inputsRef = useRef<HTMLInputElement[]>([]);
+
+  const handleSubmit = async (data: FieldValues) => {
+    console.log(data);
+  };
+  return (
+    <div className=" mt-20 flex items-center justify-center px-5">
+      <div className="border w-full lg:w-1/4 p-5 rounded-md bg-gray-50">
+        <Image
+          src={lockIcon}
+          alt="logo"
+          width={60}
+          height={66}
+          className="w-[60px] mx-auto "
+        />
+        <h1 className="text-2xl font-bold text-center py-3">
+          Enter the 6 digit OTP
+        </h1>
+
+        <form onSubmit={handleSubmit}>
+          <OTPInput
+            length={6}
+            setOTPValues={setOTPValues}
+            emptyFieldIndex={emptyFieldIndex}
+            inputsRef={inputsRef}
+          />
+          <Button
+            fullWidth
+            type="submit"
+            disabled={isLoading}
+            startIcon={isLoading ? <CircularProgress size={20} /> : null}
+            sx={{ mt: 2 }}
+          >
+            {isLoading ? "Verifying..." : "Verify"}
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default OTPVerifyPage;
