ea65043143a8046d1e1d2756010427997c8c01cf
Bablu22 <mdbablu22.bablu22@gmail.com>
2024-06-08 17:53:30 +0600

    add changes in payment page

--- src/app/(mainLayout)/payment/_components/PaymentForm.tsx
+++ src/app/(mainLayout)/payment/_components/PaymentForm.tsx
@@ -2,30 +2,41 @@
 
 import {
   Button,
+  FormControl,
   FormControlLabel,
+  InputLabel,
+  MenuItem,
   Radio,
   RadioGroup,
+  Select,
   TextField,
 } from "@mui/material";
 import { SetStateAction, useState } from "react";
 
 const PaymentForm = () => {
   const [selectedValue, setSelectedValue] = useState("bkash");
+  const [month, setMonth] = useState("");
 
-  const handleChange = (event: {
+  const handleBankChange = (event: {
     target: { value: SetStateAction<string> };
   }) => {
     setSelectedValue(event.target.value);
   };
 
+  const handleMonthChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setMonth(event.target.value);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center ">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm mt-10">
         <h2 className="text-2xl font-semibold mb-6">Payment/পেমেন্ট</h2>
         <form>
           <div className="mb-4">
             <p className="mb-2">1. Choose Bank/ব্যাংক বেছে নিন</p>
-            <RadioGroup value={selectedValue} onChange={handleChange} row>
+            <RadioGroup value={selectedValue} onChange={handleBankChange} row>
               <FormControlLabel
                 value="bkash"
                 control={<Radio color="primary" />}
@@ -41,18 +52,51 @@ const PaymentForm = () => {
           <div className="mb-4">
             <p className="mb-2">2. Copy Account Pay/অ্যাকাউন্ট পে কপি করুন</p>
             <TextField
+              label="Target Account/লক্ষ্য অ্যাকাউন্ট"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              placeholder="017XXXXXXXX"
+            />
+            <TextField
               label="Payment Amount/পরিশোধিত অর্থ"
               variant="outlined"
               fullWidth
               margin="normal"
               placeholder="1000"
             />
+            <FormControl variant="outlined" fullWidth margin="normal">
+              <InputLabel>Month/মাস</InputLabel>
+              <Select
+                value={month}
+                onChange={handleMonthChange}
+                label="Month/মাস"
+              >
+                <MenuItem value="January">January/জানুয়ারি</MenuItem>
+                <MenuItem value="February">February/ফেব্রুয়ারি</MenuItem>
+                <MenuItem value="March">March/মার্চ</MenuItem>
+                <MenuItem value="April">April/এপ্রিল</MenuItem>
+                <MenuItem value="May">May/মে</MenuItem>
+                <MenuItem value="June">June/জুন</MenuItem>
+                <MenuItem value="July">July/জুলাই</MenuItem>
+                <MenuItem value="August">August/আগস্ট</MenuItem>
+                <MenuItem value="September">September/সেপ্টেম্বর</MenuItem>
+                <MenuItem value="October">October/অক্টোবর</MenuItem>
+                <MenuItem value="November">November/নভেম্বর</MenuItem>
+                <MenuItem value="December">December/ডিসেম্বর</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="mb-4">
+            <p className="mb-2">
+              3. Your Payment Account/ আপনার পেমেন্ট অ্যাকাউন্ট
+            </p>
             <TextField
-              label="Target Account/লক্ষ্য অ্যাকাউন্ট"
+              label="Transaction ID/লেনদেন আইডি লিখুন"
               variant="outlined"
               fullWidth
               margin="normal"
-              placeholder="017XXXXXXXX"
+              placeholder="123456"
             />
           </div>
           <div className="mb-4">
@@ -66,7 +110,7 @@ const PaymentForm = () => {
             </p>
           </div>
           <div className="mb-4">
-            <p className="mb-2">3. Enter Transaction ID/লেনদেন আইডি লিখুন</p>
+            <p className="mb-2">4. Enter Transaction ID/লেনদেন আইডি লিখুন</p>
             <TextField
               label="Transaction ID/লেনদেন আইডি লিখুন"
               variant="outlined"
